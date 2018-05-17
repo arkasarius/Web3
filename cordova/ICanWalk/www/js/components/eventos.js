9bf17067b6cdbfc4f5e2bb8d7868ff6aa7683bc9
arkasarius <roger.ordonez.brumos@gmail.com>
2018-05-17 14:06:07 +0200

    funciona el override de datos desde firebase

--- cordova/ICanWalk/www/js/components/eventos.js
+++ cordova/ICanWalk/www/js/components/eventos.js
@@ -1,31 +1,61 @@
 const EventosTemplate = {
 props: [],
+created: function(){
+  var database = firebase.database();
+  var that=this;
+  database.ref("eventos").once('value').then(function(snapshot) {
+    //this.eventos=snapshot.val();
+    var x = Object.values(snapshot.val());
+    //alert(JSON.stringify(x));
+  //  alert(x[0].nom);
+    that.eventos=x;
+    //alert(JSON.stringify(snapshot.val()));
+});
+
+},
 methods:
 {
   test: function(){
     var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    li.setAttribute("id", "id1");
-    li.appendChild(document.createTextNode("hola"));
-    ul.appendChild(li);
-  },
-  asd: function() {
-      var ul = document.getElementById("list");
-      ul.innerHTML="pene";
   }
 },
 data: () => ({
-eventosLista: [
-'Orange',
-'Apple',
-'Pineapple'
-]
+eventos:[{
+  nom:"",
+  direccio:"",
+  creador:"",
+  posicio:[{
+    lat:"",
+    lon:""
+  }],
+  data:[{
+    date:"",
+    day:"",
+    hours:"",
+    minutes: "",
+    seconds:"",
+    time:"",
+    timezoneOffset:"",
+    year:""
+  }],
+  descripcioCurta:"",
+  descripcioLlarga:"",
+  direccio:"",
+  geolocalitzacio:[{
+    lat:"",
+    lon:""
+  }],
+  limitUsuaris:"",
+  usuarisRegistrats:[{
+  }],
+  preu:""
+}]
 }),
 template:
 `
 <md-list>
-  <md-list-item @click="asd()">Button</md-list-item>
-
+<md-list-item>hola</md-list-item>
+<md-list-item v-for="a in eventos" >{{a.nom}}</md-list-item>
 </md-list>
 
 `
